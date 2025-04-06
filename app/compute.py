a5c8a80738ca6858db1d53dc3818b252271a22a5
adref brahim <adref@ADREFs-Air.lan>
2025-04-06 23:21:23 +0200

    add npi compute logic

---
+++ app/compute.py
@@ -0,0 +1,40 @@
+def compute_npi(expression: str):
+    stack = []
+    elements = expression.split()
+    
+    for element in elements:
+        if element.isdigit():
+            stack.append(float(element))
+        else:
+            # Verify if we have the suffisant elemnts to integrate in the computation
+            if len(stack) < 2:
+                raise ValueError('Invalid Expression')
+            
+            # (x 'operator' y) like (x + y)
+            x = stack.pop()
+            y = stack.pop()
+
+            if element == '+':
+                stack.append(x + y)
+            elif element == '-':
+                stack.append(x - y)
+            elif element == '*':
+                stack.append(x * y)
+            elif element == '/':
+                # Verfiy the div by zero
+                if y == 0: 
+                    raise ValueError('Div by zero')
+                stack.append(x / y)
+            else: 
+                raise ValueError('Invalid Operator')
+            # Verify the len of the stack for a valid expression we will finish with one element
+    if len(stack) != 1:
+        raise ValueError('Invalid Expression')
+    return stack[0]
+
+
+
+
+if __name__ == '__main__':
+    expression = '5 2 + 16 1 * +'
+    print(compute_npi(expression))
