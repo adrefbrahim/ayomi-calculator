182e96d6d798366f5a8c67d580ac746efad8b13c
adref brahim <adref@ADREFs-Air.lan>
2025-04-07 02:04:30 +0200

    add export route and function

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 import io
 import os 
+import csv
 from fastapi import FastAPI, Depends
+from fastapi.responses import StreamingResponse
 from .compute import compute_npi
 from .db_connector import get_db, Session
 from .models import Operations
@@ -22,3 +24,16 @@ def compute(expression: str, db: Session = Depends(get_db)):
     response['expression'] = expression
     response['result'] = result
     return  response
+
+@app.get('/export/')
+def export_all(db: Session = Depends(get_db)):
+    output = io.StringIO()
+    writer = csv.writer(output)
+    writer.writerow(['id', 'expression', 'result'])
+    operations = db.query(Operations).all()
+
+    for op in operations:
+        writer.writerow([op.id, op.expression, op.result])
+    
+    output.seek(0)
+    return StreamingResponse(output, media_type='text/csv', headers={"Content-Disposition": "attachment; filename=calculations.csv"})
